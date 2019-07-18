d5cdf4f32f72a8a6dcf89cea0109edda201215e3
TrejGun <trejgun@gmail.com>
2019-07-18 14:51:13 +0400

    init

---
+++ node-app1/app.js
@@ -0,0 +1,11 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello World from Node.js app 1!!!</h1>\n");
+  response.end();
+});
+
+server.listen(8001);
